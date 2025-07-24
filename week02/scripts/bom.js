df76d7fe433aebf2e5951c6ba45649131402288d
Rotems <mymaster17@gmaiil.com>
2025-07-24 19:34:32 +0100

    week 2

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,16 +1,16 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('______'); 
+const list = document.querySelector('ul');
 const li = document.createElement('li');
 const deleteButton = document.createElement('button');
 li.textContent = input.value;
 deleteButton.textContent = '❌';
 li.append(deleteButton);
 list.append(li);
-button.addEventListener('click', function() {
-    if (input.value = '');
-  });
+button.addEventListener('click', function () {
+  if (input.value = '');
+});
 deleteButton.addEventListener('click', function () {
-    list.removeChild(li);
-    input.focus();
-  });
+  list.removeChild(li);
+  input.focus();
+});
